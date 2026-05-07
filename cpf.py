5a402b5535b2651f9f828dd8aca79cf1cf2417db
Lucas Inácio <lucasinacio.dev.br@gmail.com>
2026-05-07 20:20:15 -0300

    Projeto API de consulta de CPF

--- cpf.py
+++ cpf.py
@@ -7,7 +7,7 @@ import requests
 cpf = '48764470806'
 url = f'https://api.cpfhub.io/cpf/{cpf}'
 headers = {
-    'x-api-key': '18511129b02f5ead78a7e93ceb4e7266530176fbf49221462f72580a4643cece',
+    'x-api-key': 'api-key',
     'Accept': 'application/json'
 }
 
